d9c83c59becac528ba76debcf726374ec5555ce0
im-engineer <azmsiddhant1@gmail.com>
2023-03-14 15:52:14 +0530

    changes in server,confing, model, controller  and route

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,6 +1,16 @@
-module.exports = {
+const {createPool} = require("mysql");
+
+const pool = createPool ({
   HOST: "localhost",
   USER: "root",
   PASSWORD: "smart@2099",
-  DB: "testdb"
-};
+  DB: "testDB"
+});
+
+pool.query("select * from Persons", (err, result, field) => {
+  if (err) {
+    return console.log(err);
+  } else {
+    return console.log(result)
+  }
+})
