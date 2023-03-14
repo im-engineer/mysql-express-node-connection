7af47c92d1c7cf874c0a4aa55348b6f3f3efa288
im-engineer <azmsiddhant1@gmail.com>
2023-03-14 17:54:33 +0530

    change updated on 13 march

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,16 +1,8 @@
-const {createPool} = require("mysql");
-
-const pool = createPool ({
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "smart@2099",
-  DB: "testDB"
+var {createConnection} = require('mysql');
+var con = createConnection({
+  host: "localhost",
+  user: "root",
+  password: "smart@2099"
 });
 
-pool.query("select * from Persons", (err, result, field) => {
-  if (err) {
-    return console.log(err);
-  } else {
-    return console.log(result)
-  }
-})
+
