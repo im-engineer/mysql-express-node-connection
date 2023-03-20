a432fa8f1bbb19062a11cb642afd7330df98ed3d
im-engineer <azmsiddhant1@gmail.com>
2023-03-20 16:44:50 +0530

    update change

--- app/controllers/hl.js
+++ app/controllers/hl.js
@@ -1,4 +1,5 @@
 var hl7 = require('simple-hl7');
+///////////////////SERVER/////////////////////
 var app = hl7.tcp();
 app.use(function (req, res, next) {
     //req.msg is the HL7 message
@@ -6,16 +7,13 @@ app.use(function (req, res, next) {
     console.log(req.msg.log());
     next();
 })
-
 app.use(function (req, res, next) {
     //res.ack is the ACK
     //acks are created automatically
-
     //send the res.ack back
     console.log('******sending ack*****')
     res.end()
 })
-
 app.use(function (err, req, res, next) {
     //error handler
     //standard error middleware would be
@@ -26,256 +24,119 @@ app.use(function (err, req, res, next) {
     res.ack.addSegment('ERR', err.message);
     res.end();
 });
-
 //Listen on port 7777
 app.start(7777); //optionally pass encoding here, app.start(1234, 'latin-1');
 ///////////////////SERVER/////////////////////
-
 ///////////////////CLIENT/////////////////////
 var client = hl7.Server.createTcpClient('localhost', 7777);
-
 //create a message
-var msg = new hl7.Message(
-    "OXV",
-    "1",
-    "Local^Reciver",
-    "Local^Facility",
-    "dateTime",
+var msg = new hl7.Message("OXV", //Sending Application
+    "SendingFac", //Sending Facility
+    "ReceivingApp", // Reciving Application
+    "ReceivingFac", //Reciving Facility
+    "202203171436", //Date/Time of message
     "",
-    "ORU^R01",
-    "599102",
-    "P",
-    "2.3"
-)
-msg.addSegment("EVN",
-    "",
-    "dateTime",
+    "ORU^R01", // Message Type
+    "12345", //Message Control ID
+    "P", //Processing ID
+    "2.5",
     "",
     "",
-    "Rohan",
-)
+    "AL",
+    "NE",
+    "")
 
 msg.addSegment("PID",
-    "",
-    "",
-    "1MT^0^M10",
-    "",
-    "Patient^Jonny^Dee^^DR",
-    "Patient^Momma^Thee^^MS",
-    "20040101000000",
-    "M",
-    "",
-    "B",
-    "555 Johnson Road^Apt. 555^Indianapolis^IN^46202^USA",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "Indianapolis, IN",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-)
-
-msg.addSegment(
-    "PV1",
     "1",
-    "O",
-    "^^^^^^^^MTRH^1^AMRS",
-    "2",
+    "ABC123DF", //Patient ID
+    "AND234DA_PID3", //Patient Identifier List
+    "PID_4_ALTID", //Alternate Patient ID - PID
+    "Patlast^Patfirst^Mid", //Patient Name
+    "",
+    "19670202",  //Date/Time of Birth
+    "F",
     "",
     "",
-    "1^Mamlin^Joseph^^^^^^^^8^M10^^AMRS",
+    "4505 21 st^^LAKE COUNTRY^BC^V4V 2S7", //Patient Address
     "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
+    "222-555-8484", //Phone Number
     "",
     "",
     "",
     "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "20050217140000",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "V"
+    "MF0050356/15" //Patient Account Number
 )
-
-// msg.addSegment(
-//     "ORC",
-//     "RE",
-//     "",
-//     "",
-//     "",
-//     "",
-//     "",
-//     "",
-//     "",
-//     "20050221130000",
-//     "1^Enterer^Ima^^^^^^^^^^^AMRS"
-// )
-
-// msg.addSegment(
-//     "OBR",
-//     "1",
-//     "",
-//     "",
-//     "1238^MEDICAL RECORD OBSERVATIONS^DCT",
-//     "",
-//     "",
-//     ""
-// )
-
-// msg.addSegment(
-//     "OBX",
-//     "1",
-//     "CE",
-//     "1082^REVIEW OF SYSTEMS, CENTRAL NERVOUS SYSTEM^DCT",
-//     "",
-//     "207^DEPRESSION^DCT",
-//     "",
-//     "",
-//     "",
-//     "",
-//     "",
-//     "",
-//     "",
-//     "",
-//     "20050217204000"
-// )
-
-// msg.addSegment(
-//     "OBX",
-//     "2",
-//     "SN",
-//     "5497^CD4 COUNT^DCT",
-//     "",
-//     "<^10",
-//     "cells/mm3",
-//     "10-1500",
-//     "L",
-//     "",
-//     "",
-//     "F",
-//     "",
-//     "",
-//     "20050217204000"
-// )
-
-// msg.addSegment(
-//     "OBX",
-//     "3",
-//     "NM",
-//     "5089^WEIGHT (KG)^DCT",
-//     "",
-//     "25",
-//     "kg",
-//     "20-300",
-//     "L",
-//     "",
-//     "",
-//     "F",
-//     "",
-//     "",
-//     "20050217204000"
-// )
-
-// msg.addSegment(
-//     "OBX",
-//     "4",
-//     "TS",
-//     "1191^HISTORICAL DRUG STOP DATE^DCT",
-//     "",
-//     "20050101",
-//     "",
-//     "",
-//     "",
-//     "",
-//     "",
-//     "",
-//     "F",
-//     "",
-//     "",
-//     "",
-//     "20050217204000"
-// )
-
-// msg.addSegment(
-//     "MSA",
-//     "AR",
-//     "AMRS20050217152845"
-// )
-
-// msg.addSegment(
-//     "ERR",
-//     "",
-//     "PID^1^3^^2",
-//     "204",
-//     "E"
-// )
 
 msg.addSegment("OBR",
-    "", "", "", "", "", "", "", "20010501141500.0000", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "F", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""
+    "1",
+    "",
+    "12376", //Filler Order Number
+    "oxv^Spo2", //Universal Service Identifier
+    "R", //Priority_OBR
+    "",
+    "20120410160227", //Observation Date/Time
+    "",
+    "",
+    "22^GOOF^GOOFY", //Collector identifier
+    "",
+    "",
+    "Fasting: No",
+    "201204101625", //Specimen recieved Date/Time
+    "",
+    "71^DUCK^DONALD", //Ordering Provider
+    "",
+    "",
+    "",
+    "",
+    "",
+    "201204101630", //Results Rpt/Status Chng - Date/Time
+    "",
+    "",
+    "F", //Result Status
+    "",
+    "^^^^^R", //Quantity Time
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "85025" //Procedure Code
 )
 
 msg.addSegment("OBX",
-    "", "1", "HD", "SR Instance UID", "", "", "1.113654.1.2001.30.2.1", "", "", "", "", "", "", "F", "", "", "", "", ""
+    "1",
+    "TX",  //Value Type
+    "301.0500^Observation Type",   //Observation Identifier
+    "",
+    "Spo2",  //Observation Value
+    "",
+    "",
+    "",
+    "",
+    "",
+    "F",   //Observation Result Status 
+    ""
 )
-
-msg.addSegment("OBX","2","TX","SR Text","","Radiology Report History Cough Findings PA evaluation of the chest demonstrates the lungs to be expanded and clear.  Conclusions Normal PA chest x-ray.","","","","","","F","","","","","")
-
+msg.addSegment("OBX", "2", "TX", "301.0600^Observation Value", "", "83", "", "", "", "", "", "F", "")
+msg.addSegment("OBX", "3", "TX", "301.0700^Parameter Type", "", "range", "", "", "", "F", "")
+msg.addSegment("OBX", "4", "TX", "301.0900^Manufacturer", "", " OXV", "", "", "", "", "", "F", "")
+msg.addSegment("OBX", "5", "TX", "301.1100^Status", "", "complete", "", "", "", "", "", "F", "")
+msg.addSegment("OBX", "5", "TX", "301.1100^Type", "", "Low", "", "", "", "", "", "F", "")
+msg.addSegment("OBX", "6", "TX", "301.1300^App Version / Firmware Version", "", "1.12 / 2.15", "", "", "", "", "", "F", "")
 console.log('******sending message*****')
 client.send(msg, function (err, ack) {
     console.log('******ack received*****')
-    console.log(ack.log());
+    //   console.log(ack.log());
 });
+///////////////////CLIENT/////////////////////
